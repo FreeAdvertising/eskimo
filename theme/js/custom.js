0c52e5ef7bb35bfea746f65e101110a6d66d9a8f
Ryan Priebe <ryan@wearefree.ca>
2014-05-01 11:27:39 -0600

    started removing unnecessary files

--- theme/js/custom.js
+++ theme/js/custom.js
@@ -3,53 +3,6 @@
  * @return void
  */
 window.addEventListener("DOMContentLoaded", function(){
-	if(document.querySelector("#wpadminbar")){
-		document.body.classList.add("logged-in");
-	}else {
-		document.body.classList.remove("logged-in");
-	}
-	
-	var quoterequests = document.querySelectorAll(".request-quote"),
-		request_quote_form = document.querySelector(".request-quote-form");
-	
-	for(var i = 0; i < quoterequests.length; i++){
-		quoterequests[i].addEventListener("click", function(evt){
-			/*evt.preventDefault();*/
-			window.location.href = "#";
-	
-			if(false == jQuery(request_quote_form).is(":visible")){
-				jQuery(request_quote_form).slideDown();
-			}else {
-				jQuery(request_quote_form).slideUp();
-			}
-		});
-	}
-
-	jQuery(".request-quote-form .send-btn").click(function(evt){
-		evt.preventDefault();
-		
-		var url = this.parentElement.parentElement.parentElement.action;
-
-		jQuery("span.sending").fadeIn();
-
-		jQuery.post(url, jQuery(".request-quote-form form").serialize(), function(data){
-			jQuery("p.form-response").text(data.message);
-		}).fail(function(data){
-			jQuery("p.form-response").text(data.message);
-		}).always(function(){
-			jQuery("span.sending").fadeOut().remove();
-		});
-	});
-
-	//because you can't actually add classes to contact-form-7 input elements
-	//(bug???)
-	jQuery("body.contact form.wpcf7-form input[type='text'], body.contact form.wpcf7-form input[type='email'], body.contact form.wpcf7-form textarea").addClass("form-control");
-
-	//remove weird break tags from some pages
-	jQuery("body.gallery br").each(function(){
-		jQuery(this).remove();
-	});
-
 	//initialize detect.js
 	var info = new Detect({format: false});
 });
--- theme/views/header.php
+++ theme/views/header.php
@@ -6,24 +6,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
 
-		<title><?php echo get_bloginfo("site_name"); ?> | North America's leader 
-in pipeline and oilfield transportation</title>
+		<title><?php echo get_bloginfo("site_name"); ?></title>
 
-<meta name="description" content="Privately owned and operated, Pioneer Truck Lines Ltd. is Edmonton’s total transportation solutions company, specializing in oilfield and pipeline delivery services.">
-<meta name="keywords" content="edmonton, camrose, alberta, trucking, transportation, hauling, pipeline, stockpiling, heavy hauling, oilfield, picker, crane, logistics, storage, canada, Pipe stringing, Stockpiling pipe, Pipeline stringing, Pipelining, Pipeline construction, Pipe hauling, Hauling pipe, Hauling equipment, Northern Alberta Trucking, Alberta Trucking, Fort McMurray Trucking, Specailized transportation, Edmonton dispatchers, North Dakota pipe, North Dakota trucking, Trans Border pipe, Trans Border shipping, Sideboom Rental, Pipelayers, Pipelayer rentals, Pipe laying equipment, Sideboom haul, Stringing stick, Heavy Haulers, Edmonton transportation companies, Edmonton trucking companies, Edmonton trucking, Pipeline trucking, Pipeline trucking company, Pipeline logistics company, Construction transport, Calgary trucking, Pipe storage, Oil Sands construction, Oil sands hauling, Facilities construction logistics, Pipeline hauling, Pipe logistics, Transload, Reload, Pickers, Boom trucks, Heavy equipment haul, Alberta Logistics, Western Canadian Logistics, Pipeline consultants, Energy logistics, Energy sector logistics, Facilities transporter, Heavy construction transporter, Energy facility logistics, Hauling pipe, Stringing pipe, Pipeline spread transportation, Line Pipe, Coated pipe, Pole trailers, Mainline construction, Spool hauling, Pipe spool hauling, Valve transportation, Pipeline installation, Oilfield hauling, Oil and Gas logistics, Bridge Beam hauling, Specialty loads, Rig mat hauling, 80 foot pipe, Double jointed pipe, Ice road trucking, Camp moves, Pipe yard, Pipe transportation, Low bed, Lowbedding, Flat deck hauling, Crane services">
-
-		<!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>  -->
-
-<meta property="og:title" content="Pioneer Trucklines"/>
-<meta property="og:type" content="website"/>
-<meta property="og:url" content="http://www.pioneertrucklines.com/"/>
-<meta property="og:image" content="http://pioneer.wearefree.ca/wp-content/uploads/2014/03/Pioneer_logo.jpg"/>
-<meta property="og:site_name" content="Pioneer Trucklines"/>
-<meta property="og:description" content="Privately owned and operated, Pioneer Truck Lines Ltd. is Edmonton’s total transportation solutions company, specializing in oilfield and pipeline delivery services."/>
-
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
-
-
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	</head>
 	
 	<body <?php body_class(); ?>>
@@ -57,34 +42,13 @@ in pipeline and oilfield transportation</title>
 					</div>
 				</form>
 			</div>
-
-			<div class="container">
-				<div class="row">
-					<nav class="col-md-3">
-						<ul class="social-icons">
-							<li><a href="https://www.facebook.com/pages/Pioneer-Truck-Lines-Ltd/678286332234829?ref=hl" target="_blank" class="facebook" title="Facebook">Facebook</a></li>
-							<li><a href="https://www.youtube.com/channel/UCtMFnAU6-jP0Oov5K3QHPRA" target="_blank" class="youtube" title="Youtube">Youtube</a></li>
-							<li><a href="https://twitter.com/PioneerTrucking" target="_blank" class="twitter" title="Twitter">Twitter</a></li>
-							<li><a href="http://www.linkedin.com/profile/view?id=333039844&trk=nav_responsive_tab_profile" target="_blank" class="linkedin" title="Linkedin">Linkedin</a></li>
-							<li><a href="mailto:general@pioneertrucklines.com?Subject=Website%20Quote%20Request" target="_top"" class="email" title="Email">Email</a></li>
-						</ul>
-					</nav>
-
-					<nav class="col-md-9">
-						<ul>
-							<li class="request-a-quote"><a href="#" class="request-quote">Request A Quote</a></li>
-							<li class="toll-free">Call us Toll Free 1-800-315-3148</li>
-							<li class="search"><?php get_search_form(); ?></li>
-						</ul>
-					</nav>
-				</div>
-			</div>
 		</header>
 
 	
 		<div class="custom-container">
 		<div id="headerBg">
-		<div id="squares">			<header class="top">
+		<div id="squares">			
+			<header class="top">
 				<div class="container">
 					<div class="row">
 						<div class="top-logo col-md-2">
