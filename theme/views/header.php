bd07fbb1649b46573f8167491330614f995a0553
Ryan Priebe <ryan@wearefree.ca>
2014-05-06 17:14:29 -0600

    Fixed some silly indenting/element closing issues in header.php

--- theme/views/header.php
+++ theme/views/header.php
@@ -10,68 +10,33 @@
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	</head>
 	
-	<body <?php body_class(); ?>>
-		<header class="meta">
-			<div class="request-quote-form container" style="display: none;">
-				<form action="<?php echo get_template_directory_uri(); ?>/api/forms/requestquote" method="post" role="form">
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="name">Name</label>
-							<input type="text" name="name" id="name" class="form-control" />
-						</div>
-
-						<div class="form-group">
-							<label for="email">Email</label>
-							<input type="email" name="email" id="email" class="form-control" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="message">Message</label>
-							<textarea class="form-control" id="message" name="message"></textarea>
-						</div>
-
-						<div class="form-group">
-							<p class="form-response"><span class="sending">Sending...</span></p>
-						</div>
-
-						<div class="form-group">
-							<button class="btn btn-default send-btn">Send</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</header>
-
-	
+	<body <?php body_class(); ?>>	
 		<div class="custom-container">
-		<div id="headerBg">
-		<div id="squares">			
-			<header class="top">
-				<div class="container">
-					<div class="row">
-						<div class="top-logo col-md-2">
-							<h1 class="logo"><a href="<?php bloginfo('url'); ?>" style="background-image: url('<?php echo get_template_directory_uri(); ?>/theme/images/logo.png');"><?php echo get_bloginfo("site_name"); ?></a></h1>
+			<div id="headerBg">
+				<div id="squares">
+					<header class="top">
+						<div class="container">
+							<div class="row">
+								<div class="top-logo col-md-2">
+									<h1 class="logo"><a href="<?php bloginfo('url'); ?>" style="background-image: url('<?php echo get_template_directory_uri(); ?>/theme/images/logo.png');"><?php echo get_bloginfo("site_name"); ?></a></h1>
+								</div>
+
+								<div class="top-menu col-md-10">
+									<?php 
+										//desktop menu
+										wp_nav_menu(array(
+											"theme_location" => "primary",
+											"container" => "nav",
+											"container_class" => "desktop-menu clearfix hidden-xs",
+											)); 
+									?>
+								</div>
+							</div>
+								<?php echo \Free\System\SectionFactory::bannerWidgets(); ?>
 						</div>
-
-						<div class="top-menu col-md-10">
-							<?php 
-								//desktop menu
-								wp_nav_menu(array(
-									"theme_location" => "primary",
-									"container" => "nav",
-									"container_class" => "desktop-menu clearfix hidden-xs",
-									)); 
-							?>
-						</div>
-					</div>
-						<?php echo \Free\System\SectionFactory::bannerWidgets(); ?>
+					</header>
 				</div>
-				</div>
-				</div>
-
-			</header>
-
+			</div>
 			
 			<header class="navbar navbar-default navbar-fixed-top hidden-lg hidden-md hidden-sm">
 				<div class="navbar-header container">
